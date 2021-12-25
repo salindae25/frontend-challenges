d69786778ab21cc64d9ac66999c527da611e18d8
salindae25 <salindae25@gmail.com>
2021-12-25 15:31:34 +0530

    E-commerce product page mobile view, static contents and initial config

--- src/lib/job/JobFilter.svelte
+++ src/lib/job/JobFilter.svelte
@@ -1,31 +1,25 @@
 <script lang="ts">
-	import { getContext } from 'svelte';
-	const { toggleCategories, clearFilter } = getContext('jobList');
-	export let categories: string[] = [];
+  import { getContext } from "svelte";
+  const { toggleCategories, clearFilter } = getContext("jobList");
+  export let categories: string[] = [];
 </script>
 
 <section
-	class="
-        bg-white
-        flex
-        p-4
-        rounded-[8px]
-        shadow-lg shadow-job-cyan-600/50
-        gap-4
-        relative
-        md:flex-row md:item-center md:px-8
-        top-[-30px]
-        flex-wrap
-        w-5/6
-        mx-auto
-        min-h-xs
-      "
+  class="bg-white flex p-4 rounded-[8px] shadow-lg shadow-job-cyan-600/50 gap-4 relative md:flex-row md:item-center md:px-8 top-[-30px] flex-wrap w-5/6 mx-auto min-h-xs"
 >
-	{#each categories as category}
-		<div class="flex rounded item-center">
-			<span class="bg-job-cyan-200 text-job-cyan-500 py-1 px-2 font-bold">{category}</span>
-			<button class="bg-job-cyan-500 w-8 text-white rounded-r font-bold bg-[url('/images/icon-remove.svg')] bg-no-repeat bg-center hover:bg-job-cyan-800" on:click="{() => toggleCategories(category)}"></button>
-		</div>
-	{/each}
-	<button class="text-job-cyan-500 underline ml-auto font-medium" on:click="{clearFilter}">Clear</button>
+  {#each categories as category}
+    <div class="flex rounded item-center">
+      <span class="bg-job-cyan-200 text-job-cyan-500 py-1 px-2 font-bold"
+        >{category}</span
+      >
+      <button
+        class="bg-job-cyan-500 w-8 text-white rounded-r font-bold bg-[url('/images/icon-remove.svg')] bg-no-repeat bg-center hover:bg-job-cyan-800"
+        on:click={() => toggleCategories(category)}
+      />
+    </div>
+  {/each}
+  <button
+    class="text-job-cyan-500 underline ml-auto font-medium"
+    on:click={clearFilter}>Clear</button
+  >
 </section>
---
+++ src/routes/e-comm/index.svelte
@@ -0,0 +1,51 @@
+<div class="w-screen min-h-screen bg-white font-e-comm-font ">
+	<nav class=" flex py-6 px-5 w-full justify-between">
+		<div class="self-start flex gap-4">
+			<i class="bg-[url('/e-comm-images/icon-menu.svg')] h-6 w-6 bg-center bg-cover"></i>
+			<img src="/e-comm-images/logo.svg" class="h-6 w-auto" alt="logo" />
+		</div>
+		<div class="self-end flex gap-4">
+			<i class="bg-[url('/e-comm-images/icon-cart.svg')] h-6 w-6 bg-center bg-cover"></i>
+			<img src="/e-comm-images/image-avatar.png" class="h-6 w-auto" alt="avatar" />
+		</div>
+	</nav>
+	<div class="h-64 bg-orange-400">
+		<!-- galerry -->
+	</div>
+	<article class="flex flex-col px-5 py-5 gap-4">
+		<h6 class="uppercase text-e-comm-orange font-bold text-sm tracking-wider">Sneaker Company</h6>
+		<h4 class="capitalize text-3xl font-bold text-e-comm-gray-blue-800">Fall Limited Edition Sneakers</h4>
+		<p class="text-e-comm-gray-blue-600">These low-profile sneakers are your perfect casual wear companion. Featuring a durable rubber outer sole, they’ll withstand everything the weather can offer.</p>
+		<div class="flex items-center gap-4 w-full">
+			<div class="text-3xl font-bold text-e-comm-gray-blue-800">$125.00</div>
+			<div class="text-e-comm-orange font-bold text-sm bg-e-comm-orange-100 py-1 px-2 rounded-md">50%</div>
+			<div class="text-e-comm-gray-blue-400  line-through font-bold ml-auto ">$250.00</div>
+		</div>
+		<div class="flex bg-e-comm-gray-blue-100 w-full rounded-md justify-evenly py-2">
+			<button class="rounded-l-md  h-8 flex items-center  justify-start"
+				><svg width="11" height="11" xmlns="http://www.w3.org/2000/svg" class="hover:stroke-comment-blue hover:stroke-1"
+					><path
+						d="M6.33 10.896c.137 0 .255-.05.354-.149.1-.1.149-.217.149-.354V7.004h3.315c.136 0 .254-.05.354-.149.099-.1.148-.217.148-.354V5.272a.483.483 0 0 0-.148-.354.483.483 0 0 0-.354-.149H6.833V1.4a.483.483 0 0 0-.149-.354.483.483 0 0 0-.354-.149H4.915a.483.483 0 0 0-.354.149c-.1.1-.149.217-.149.354v3.37H1.08a.483.483 0 0 0-.354.15c-.1.099-.149.217-.149.353v1.23c0 .136.05.254.149.353.1.1.217.149.354.149h3.333v3.39c0 .136.05.254.15.353.098.1.216.149.353.149H6.33Z"
+						fill="#C5C6EF"></path></svg
+				></button
+			>
+			<span class="font-medium w-8 my-auto text-center text-comment-blue">0</span>
+			<button class="rounded-r-md  flex items-center justify-center w-8 h-8 ">
+				<svg width="11" height="3" xmlns="http://www.w3.org/2000/svg" class="hover:stroke-comment-blue hover:stroke-1">
+					<path d="M9.256 2.66c.204 0 .38-.056.53-.167.148-.11.222-.243.222-.396V.722c0-.152-.074-.284-.223-.395a.859.859 0 0 0-.53-.167H.76a.859.859 0 0 0-.53.167C.083.437.009.57.009.722v1.375c0 .153.074.285.223.396a.859.859 0 0 0 .53.167h8.495Z" fill="#C5C6EF"></path>
+				</svg>
+			</button>
+		</div>
+		<button class="w-full flex items-center text-center bg-e-comm-orange gap-4 font-bold text-sm text-white py-3 justify-center rounded-md shadow-lg shadow-e-comm-orange/30">
+			<i class="h-5 text-white ">
+				<svg width="22" height="20" xmlns="http://www.w3.org/2000/svg" class="aspect-auto "
+					><path
+						class="fill-white"
+						d="M20.925 3.641H3.863L3.61.816A.896.896 0 0 0 2.717 0H.897a.896.896 0 1 0 0 1.792h1l1.031 11.483c.073.828.52 1.726 1.291 2.336C2.83 17.385 4.099 20 6.359 20c1.875 0 3.197-1.87 2.554-3.642h4.905c-.642 1.77.677 3.642 2.555 3.642a2.72 2.72 0 0 0 2.717-2.717 2.72 2.72 0 0 0-2.717-2.717H6.365c-.681 0-1.274-.41-1.53-1.009l14.321-.842a.896.896 0 0 0 .817-.677l1.821-7.283a.897.897 0 0 0-.87-1.114ZM6.358 18.208a.926.926 0 0 1 0-1.85.926.926 0 0 1 0 1.85Zm10.015 0a.926.926 0 0 1 0-1.85.926.926 0 0 1 0 1.85Zm2.021-7.243-13.8.81-.57-6.341h15.753l-1.383 5.53Z"
+						fill="#69707D"
+						fill-rule="nonzero"></path></svg
+				></i
+			><span>Add to cart</span>
+		</button>
+	</article>
+</div>
